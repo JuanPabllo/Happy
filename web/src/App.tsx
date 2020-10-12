4605dbb78d3215da034b41468dbd7616257c773a
Juan Pablo <juanpablo192.dreaw@gmail.com>
2020-10-12 12:36:59 -0300

    Create Landing Page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,33 @@
 import React from "react";
+import { FiArrowRight } from "react-icons/fi";
+
+import "./styles/global.css";
+import "./styles/pages/landing.css";
+
+import logoImg from "./images/Logo.svg";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
-  );
+    return (
+        <div id="page-landing">
+            <div className="content-wrapper">
+                <img src={logoImg} alt="Logo Happy" />
+
+                <main>
+                    <h1>Leve felicidade para o mundo</h1>
+                    <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+                </main>
+
+                <div className="location">
+                    <strong>Imperatriz</strong>
+                    <span>Maranhão</span>
+                </div>
+
+                <a href="" className="enter-app">
+                    <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)" />
+                </a>
+            </div>
+        </div>
+    );
 }
 
 export default App;
