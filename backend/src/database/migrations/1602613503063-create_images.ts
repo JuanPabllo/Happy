c43a041422697a0c0c69a47f04588f6b2e2a74fb
Juan Pablo <juanpablo192.dreaw@gmail.com>
2020-10-13 15:51:50 -0300

    criando banco de imagens

---
+++ backend/src/database/migrations/1602613503063-create_images.ts
@@ -0,0 +1,43 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class createImages1602613503063 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "images",
+        columns: [
+          {
+            name: "id",
+            type: "integer",
+            unsigned: true,
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: "increment",
+          },
+          {
+            name: "path",
+            type: "varchar",
+          },
+          {
+            name: "orphanage_id",
+            type: "integer",
+          },
+        ],
+        foreignKeys: [
+          {
+            name: "imageOrphanage",
+            columnNames: ["orphanage_id"],
+            referencedTableName: "orphanages",
+            referencedColumnNames: ["id"],
+            onUpdate: "CASCADE", //atualiza o id assim que as imagens forem atualizadas
+            onDelete: "CASCADE", //Deleta o id assim que as imagens forem deletadas
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("imagens");
+  }
+}
