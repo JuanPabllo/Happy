247245c1fac29cd26214ee1c74d0d4ec5b1fed89
Juan Pablo <juanpablo192.dreaw@gmail.com>
2020-10-13 12:39:11 -0300

    Createc config DataBase and TypeORM

---
+++ backend/src/models/Orphanage.ts
@@ -0,0 +1,17 @@
+export default class Orphanage {
+  id: number;
+
+  name: string;
+
+  latidude: number;
+
+  longitude: number;
+
+  about: string;
+
+  instructions: string;
+
+  opening_hours: string;
+
+  open_on_weekends: boolean;
+}
