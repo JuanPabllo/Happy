84e7c8d16f3ac028a9ea0fa5cec2dd660b11d876
Juan Pablo <juanpablo192.dreaw@gmail.com>
2020-10-14 11:24:01 -0300

    validation datas

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -1,13 +1,34 @@
 import { Request, Response } from "express";
-// import * as Yup from 'yup';
-// import CreateOrphanageService from '../services/CreateOrphanageService';
-// import ListOrphanagesService from '../services/ListOrphangesService';
-// import ShowOrphanageService from '../services/ShowOrphanageService';
-
+import { getRepository } from "typeorm";
 import orphanageView from "../views/orphanages_view";
+import * as Yup from "yup";
 
-export default class OrphanagesController {
-  async create(request: Request, response: Response): Promise<Response> {
+import Orphanage from "../models/Orphanage";
+
+export default {
+  async index(request: Request, response: Response) {
+    const orphanagesRepository = getRepository(Orphanage);
+
+    const orphanages = await orphanagesRepository.find({
+      relations: ["images"],
+    });
+
+    return response.json(orphanageView.renderMany(orphanages));
+  },
+
+  async show(request: Request, response: Response) {
+    const { id } = request.params;
+
+    const orphanagesRepository = getRepository(Orphanage);
+
+    const orphanage = await orphanagesRepository.findOneOrFail(id, {
+      relations: ["images"],
+    });
+
+    return response.json(orphanageView.render(orphanage));
+  },
+
+  async create(request: Request, response: Response) {
     const {
       name,
       latitude,
@@ -18,8 +39,11 @@ export default class OrphanagesController {
       open_on_weekends,
     } = request.body;
 
+    const orphanagesRepository = getRepository(Orphanage);
+
     const requestImages = request.files as Express.Multer.File[];
-    const images = requestImages.map((image: Express.Multer.File) => {
+
+    const images = requestImages.map((image) => {
       return { path: image.filename };
     });
 
@@ -34,45 +58,29 @@ export default class OrphanagesController {
       images,
     };
 
-    // const schema = Yup.object().shape({
-    //   name: Yup.string().required(),
-    //   latitude: Yup.number().required(),
-    //   longitude: Yup.number().required(),
-    //   about: Yup.string().required().max(300),
-    //   instructions: Yup.string().required(),
-    //   opening_hours: Yup.string().required(),
-    //   open_on_weekends: Yup.boolean().required(),
-    //   images: Yup.array(
-    //     Yup.object().shape({
-    //       path: Yup.string().required(),
-    //     }),
-    //   ),
-    // });
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      latitude: Yup.number().required(),
+      longitude: Yup.number().required(),
+      about: Yup.string().required().max(300),
+      instructions: Yup.string().required(),
+      opening_hours: Yup.string().required(),
+      open_on_weekends: Yup.boolean().required(),
+      images: Yup.array(
+        Yup.object().shape({
+          path: Yup.string().required(),
+        })
+      ).required(),
+    });
 
-    // await schema.validate(data, {
-    //   abortEarly: false,
-    // });
+    await schema.validate(data, {
+      abortEarly: false,
+    });
 
-    const createOrphanageService = new CreateOrphanageService();
-    const orphanage = await createOrphanageService.execute(data);
+    const orphanage = orphanagesRepository.create(data);
+
+    await orphanagesRepository.save(orphanage);
 
     return response.status(201).json(orphanage);
-  }
-
-  async list(request: Request, response: Response): Promise<Response> {
-    const listOrphanagesService = new ListOrphanagesService();
-
-    const orphanages = await listOrphanagesService.execute();
-
-    return response.json(orphanageView.renderMany(orphanages));
-  }
-
-  async show(request: Request, response: Response): Promise<Response> {
-    const { id } = request.params;
-
-    const showOrphanageService = new ShowOrphanageService();
-    const orphanage = await showOrphanageService.execute(Number(id));
-
-    return response.json(orphanageView.render(orphanage));
-  }
-}
+  },
+};
--- backend/src/views/orphanages_view.ts
+++ backend/src/views/orphanages_view.ts
@@ -3,28 +3,16 @@ import imagesView from "./images_view";
 
 export default {
   render(orphanage: Orphanage) {
-    const {
-      id,
-      name,
-      latitude,
-      longitude,
-      about,
-      instructions,
-      opening_hours,
-      open_on_weekends,
-      images,
-    } = orphanage;
-
     return {
-      id,
-      name,
-      latitude,
-      longitude,
-      about,
-      instructions,
-      opening_hours,
-      open_on_weekends,
-      images: imagesView.renderMany(images),
+      id: orphanage.id,
+      name: orphanage.name,
+      latitude: orphanage.latitude,
+      longitude: orphanage.longitude,
+      about: orphanage.about,
+      instructions: orphanage.instructions,
+      opening_hours: orphanage.opening_hours,
+      open_on_weekends: orphanage.open_on_weekends,
+      images: imagesView.renderMany(orphanage.images),
     };
   },
 
